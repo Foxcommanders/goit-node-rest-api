163096320b98232f90b424c39d1a6fc30daf8871
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-28 13:20:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
-import contactsRouter from "./routes/contactsRouter.js";
+const { DB_HOST } = process.env;
+
+const contactsRouter = require("./routes/contactsRouter");
 
 const app = express();
 
@@ -19,8 +23,18 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
+  next();
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
