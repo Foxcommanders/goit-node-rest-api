08c385fd671b736e789e24feee460d146403a8ca
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-28 13:27:12 +0200

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,25 @@
-import Joi from "joi";
+const Joi = require('joi')
 
-export const createContactSchema = Joi.object({
-
+const createContactSchema = Joi.object({
+    name: Joi.string().required(),
+    email:Joi.string().required(),
+    phone:Joi.number().required(),
+    
 })
 
-export const updateContactSchema = Joi.object({
-
+const updateContactSchema = Joi.object({
+    name: Joi.string().required(),
+    email:Joi.string().required(),
+    phone:Joi.string().required(),
+    
 })
+
+const updatefavoriteSchema = Joi.object({
+    favorite:Joi.boolean().required(),
+})
+
+module.exports = {
+    createContactSchema,
+    updateContactSchema,
+    updatefavoriteSchema
+}
