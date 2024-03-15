5a32efe39161fcd339d969319ec0c6670f76b417
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-03-15 13:59:47 +0200

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const validateBody = require("../helpers/validateBody");
+const sсhema = require("../schemas/contactsSchemas");
+const controller = require("../controllers/auth/register");
+const controllerLogin = require("../controllers/auth/login");
+
+router.post(
+  "/register",
+  validateBody(sсhema.userSchema),
+  controller.registration
+);
+router.post("/login", validateBody(sсhema.userSchema), controllerLogin.login);
+
+module.exports = router;
