871df34b6bbd0d95f575602e75ffb8a000ba77b1
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-03-15 13:59:32 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,22 +1,29 @@
-const { Schema ,model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const contactsSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-},{versionKey:false,timestamps:true});
+  { versionKey: false, timestamps: true }
+);
 
-const Contact = model('contact',contactsSchema)
+const Contact = model("contact", contactsSchema);
 
-module.exports = Contact
+module.exports = Contact;
