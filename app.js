5a82066ddb1499084a66016b184671982deab3d3
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-03-15 13:58:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,23 +7,31 @@ const dotenv = require("dotenv").config();
 const { DB_HOST } = process.env;
 
 const contactsRouter = require("./routes/contactsRouter");
-
+const authRouter = require("./routes/auth");
 const app = express();
+const usersRouter = require("./routes/usersRouter");
 
+
+
+app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cors());
-app.use(express.json());
+
+
 
 app.use("/api/contacts", contactsRouter);
+app.use("/users", authRouter);
+app.use("/users", usersRouter);
+
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = "Server error", name } = err;
+
   res.status(status).json({ message });
-  next();
 });
 
 mongoose
