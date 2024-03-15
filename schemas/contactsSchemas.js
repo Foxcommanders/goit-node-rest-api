5dd346972594342419dc4ceee247534018bea924
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-03-15 13:38:03 +0200

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,23 @@
-import Joi from "joi";
+const Joi = require('joi');
 
-export const createContactSchema = Joi.object({
+const createContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
-})
+const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+}).min(1).message('Body must have at least one field');
 
-export const updateContactSchema = Joi.object({
+const updatefavoriteSchema = Joi.object({
+    favorite:Joi.boolean().required(),
+});
 
-})
+module.exports = {
+    createContactSchema,
+    updateContactSchema,
+    updatefavoriteSchema,
+};
