8d060fba0642c397af227552bbfd24f35478b1d2
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-03-18 11:44:40 +0200

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const { HttpError } = require("../helpers/HttpError");
+const User = require('../models/users')
+
+
+const logout = async (req, res, next) => {
+  const userId = req.user._id;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return next(HttpError(401, "Not authorized"));
+    }
+
+    user.token = null;
+    await user.save();
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getCurrentUser = async (req, res, next) => {
+  const token = req.user.token
+  const userId = req.user._id;
+
+  if(!token){
+    return next(HttpError(401, "Not token"));
+  }
+
+  try {
+    const user = await User.findById(userId);
+  
+    if (!user) {
+      return next(HttpError(401, "Not authorized"));
+    }
+  
+    res.json({
+      email: user.email,
+      subscription: user.subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { logout, getCurrentUser };
