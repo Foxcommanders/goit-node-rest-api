9ea5743cb91a92b5be26cbe9e5cbef0068914452
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-03-18 11:44:43 +0200

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,110 @@
-import contactsService from "../services/contactsServices.js";
+// const contacts = require("../services/contactsServices");
+const {HttpError} = require("../helpers/HttpError");
+const Contact = require("../models/contacts");
+const mongoose = require("mongoose");
+const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await Contact.find({});
 
-export const getAllContacts = (req, res) => {};
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw HttpError(400, "Invalid contact ID");
+    }
+    const result = await Contact.findById(id);
 
-export const deleteContact = (req, res) => {};
+    if (!result) {
+      throw HttpError(404, "Contact not found");
+    }
 
-export const createContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+const createContact = async (req, res, next) => {
+  try {
+    if (!req.body) {
+      throw HttpError(400, "Request body is missing");
+    }
+
+    const result = await Contact.create(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw HttpError(400, "Invalid contact ID");
+    }
+    const { name, email, phone } = req.body;
+    const result = await Contact.findByIdAndUpdate(
+      id,
+      { name, email, phone },
+      { new: true }
+    );
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw HttpError(400, "Invalid contact ID");
+    }
+    const result = await Contact.findOneAndDelete({ _id: id });
+
+    if (!result) {
+      throw HttpError(404);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateFavorite = async (req, res, next) => {
+  const { favorite } = req.body;
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw HttpError(400, "Invalid contact ID");
+  }
+  const result = await Contact.findByIdAndUpdate(id, favorite, { new: true });
+
+  try {
+    if (!result) {
+      throw HttpError(404, `Contact with id = ${id} not found`);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+  updateFavorite,
+};
